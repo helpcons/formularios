763ee69b84466ba67ce1fc81883cabe393da1682
Helder Pereira <helder_leg@hotmail.com>
2024-04-13 15:53:03 -0300

    adicionando models no formulário adicionar conta

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,27 @@
+const modalBudgetArea = document.getElementById("modal-budget-area")
+const modalBudgetAreaButton = document.getElementById("modal-budget-area-button")
+
+modalBudgetArea.onclick = function () {
+    modalBudgetAreaButton.showModal()
+}
+
+
+const modalCostCenter = document.getElementById("modal-cost-center")
+const modalCostCenterButton = document.getElementById("modal-cost-center-button")
+
+modalCostCenter.onclick = function () {
+    modalCostCenterButton.showModal()
+}
+
+
+const modalDre = document.getElementById("modal-dre")
+const modal = document.querySelector("dialog")
+
+modalDre.onclick = function () {
+    modal.showModal()
+}
+
+
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
